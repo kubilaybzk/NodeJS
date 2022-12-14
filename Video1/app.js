9d47d03c93b2c2097ee928d1436732219f307dbb
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2022-12-14 15:44:54 +0300

    Initial commit

---
+++ Video1/app.js
@@ -0,0 +1,37 @@
+const http=require('http'); // import işlemi
+
+http.createServer(); bu şeklilde bir server oluşturabiliyoruz.
+bu server  bir fonksiyona ihtiyaç duyar ve bu fonksiyon
+
+
+
+function rqListener(req,res){
+    //Request Listiner gelen requestleri dinlemek için
+}
+
+bu fonksiyon'un başka türlüde kullanabiliriz.
+
+
+let server = http.createServer(function(req,res){}); gibi 
+
+let server = http.createServer((req,res)=>{}); gibi 
+
+
+Şimdi bir server geliştirelim.
+
+
+
+let server = http.createServer((req,res)=>{
+    console.log(req)
+});  
+
+//Şimdi bunu Node app.js yazarak terminal üzerinden görmek isteyelim.
+//Burada eğer bu işlemi yaparsak sunucu olmadığından terminalde hiçbir şey görmeyeceğiz.
+//bunun için oluşturduğumuz sunucunun bir port üzerinden çalışması lazım .
+// bunun için 
+
+server.listen(3000);
+//Şimdi tekrar node app.js yazarsak pointer elemanının yanıp sanmadığını fark edeceğiz bu şu anlama geliyor.
+//Loop olarak bizim bütün request değerlerimizi dinliyor.
+//Şimdi bir tarayıcı açalım ve localhost:3000'e girelim.
+//Daha sonra terminal çıktısına bakalım.
